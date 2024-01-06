3f6167c2c0403975685e45a2801f556ff351e506
Divya Sehgal <divyasehgal2020@gmail.com>
2024-01-06 11:06:37 +0530

    first

---
+++ Todo/README.md
@@ -0,0 +1,46 @@
+# Do More
+
+> "_Plan less. Do more!_"
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/dbe9827b-eb3b-48ce-a661-5b58be3c77c2/deploy-status)](https://app.netlify.com/sites/epic-feynman-0d2137/deploys)
+
+- A simple SPA, based upon minimalism theme, that lets user to jot down immediate tasks to be done and has ability mark them as either completed or incompleted.
+- Encourages user with get-it-done approach.
+- Visit - [Do More](https://epic-feynman-0d2137.netlify.app)
+
+## Tech Used
+
+- ReactJs (Use of Hooks, List Rendering, States and Props)
+- CSS
+- HTML
+
+## Steps To Run
+
+- This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+- In the project directory, you can run:
+
+```
+npm install
+```
+
+```
+npm start
+```
+
+- Open [http://localhost:3000](http://localhost:3000) to view it in the browser. Runs the app in the development mode.
+- To deploy
+
+```
+npm run build
+```
+
+```
+./build
+```
+
+---
+
+## Author
+
+- Name - Abhinav
+- GitHub - [github.com/abhinavg916](https://github.com/abhinavg916)
---
+++ Todo/src/App.js
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Form from "./components/Form";
+import TodoList from "./components/TodoList.js";
+
+function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+  const [isErr, setIsErr] = useState(false);
+  useEffect(() => {
+    // Runs only once when app starts
+    getLocalTodos();
+  }, []);
+
+  useEffect(() => {
+    const filterHandler = () => {
+      switch (status) {
+        case "completed":
+          setFilteredTodos(todos.filter((todo) => todo.completed === true));
+          break;
+        case "uncompleted":
+          setFilteredTodos(todos.filter((todo) => todo.completed === false));
+          break;
+        default:
+          setFilteredTodos(todos);
+          break;
+      }
+    };
+
+    // Saving to local
+    const saveLocalTodos = () => {
+      localStorage.setItem("todos", JSON.stringify(todos));
+    };
+
+    filterHandler();
+    saveLocalTodos();
+  }, [todos, status]);
+
+  // Checking in local
+  const getLocalTodos = () => {
+    if (localStorage.getItem("todos") === null) {
+      localStorage.setItem("todos", JSON.stringify([]));
+    } else {
+      let todoLocal = JSON.parse(localStorage.getItem("todos"));
+      setTodos(todoLocal);
+    }
+  };
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Do More</h1>
+      </header>
+      <p>
+        <em>Plan less. Do more!</em>
+      </p>
+
+      <Form
+        inputText={inputText}
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+        setStatus={setStatus}
+        setIsErr={setIsErr}
+      />
+      <TodoList
+        filteredTodos={filteredTodos}
+        setTodos={setTodos}
+        todos={todos}
+      />
+      <p>
+        <em>{isErr ? "Input is empty" : ""}</em>
+      </p>
+    </div>
+  );
+}
+
+export default App;
